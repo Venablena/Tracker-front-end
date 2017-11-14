97308c96a799a269a3b326830ff4938276ff817f
Lena Venable <venablena@gmail.com>
2017-11-13 17:48:11 -0800

    add list view of all logs

--- templates/table.js
+++ templates/table.js
@@ -1,16 +1,22 @@
 function createTable(item){
   return`
   <tr>
+    <td></td>
     <td>${item.date}</td>
     <td>${item.time}</td>
-    <td>${item.location}</td>
+    <td>${item.long} / ${item.lat}</td>
     <td>${item.tag}</td>
+    <td><i class="fa fa-times fa-lg"></i></td>
     <td>
-      <span class="fa-stack fa-lg">
-        <i class="fa fa-map-marker fa-stack-2x"></i>
-        <i class="fa fa-times fa-stack-1x"></i>
-      </span>
+        <i class="fa fa-map-marker fa-lg"></i>
+        <i class="fa fa-plus"></i>
     <td>
   </tr>
   `
 }
+
+
+// <span class="fa-stack fa-lg">
+//   <i class="fa fa-map-marker fa-stack-2x"></i>
+//   <i class="fa fa-plus-circle fa-stack-1x fa-inverse"></i>
+// </span>
