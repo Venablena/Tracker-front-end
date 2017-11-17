efd0287841aeef4f6a944e76a2b745cc77c11e74
Lena Venable <venablena@gmail.com>
2017-11-16 20:13:22 -0800

    add CNAME file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const localhostURL = 'http://localhost:3000/logs'
-const herokuURL = 'https://tracker-app.herokuapp.com/logs'
+const herokuURL = 'http://put-a-pin-on-it.herokuapp.com/'
 const baseURL = window.location.href.includes('herokuapp') ? herokuURL : localhostURL
 
 allLogs()
