21cc528bbc45239e77164ff82470e6cefa9319be
Wes Reid <bwreid@gmail.com>
2017-12-05 13:39:44 -0800

    Update refreshLogs.js

--- render/refreshLogs.js
+++ render/refreshLogs.js
@@ -1,5 +1,8 @@
 function refresh(){
   console.log("refresh logs");
+  // Since we now have the back-end, we can hide some of this information there!
+  // For example, we could make a request to a route on the back-end where
+  // this information is hidden there.
   Request.getSheet('1e1GkbibPFIxJEyuWQsZnudRE2q3rQmTppUXr7-wrP1w', 'sheet1')
   .then((result) => {
     console.log(result.data);
