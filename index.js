809093c5bf85617550588ff612390cb07692bb42
Lena Venable <venablena@gmail.com>
2017-12-27 10:30:58 -0800

    retrieve data from google sheets

--- index.js
+++ index.js
@@ -11,27 +11,15 @@ createMap()
 function refreshDB(){
   Request.getSheets('1e1GkbibPFIxJEyuWQsZnudRE2q3rQmTppUXr7-wrP1w', 'sheet1')
     .then((result) => {
-      const values =  convert(result.data.values)
+      const values =  convertLog(result.data.values)
       Request.addLogs(values)
     })
     // .then(() => {
     //   Request.clearLogs('1e1GkbibPFIxJEyuWQsZnudRE2q3rQmTppUXr7-wrP1w', 'sheet1')
     //})
-  // Request.getSheets('1BSRNmpm3OIIWP2qqZ7SN2ONdTNRPWdNNsKFHVO0JhDE', 'sheet1')
-  //   .then((result) => {
-  //     const values =  convert(result.data.values)
-  //     Request.addLogs(values)
-  //   })
-  // Request.getSheets('1tLuTAy3gFu3iyOaeps9SMXR06HaGZCStHT32ju5Ni4U', 'sheet1')
-  //   .then((result) => {
-  //     const values =  convert(result.data.values)
-  //     Request.addLogs(values)
-  //   })
 }
 
-
-
-function convert(valuesArr){
+function convertLog(valuesArr){
   return valuesArr.map(array => {
   return {tag: array[0], dateTime: array[1], long: array[2], lat: array[3]}
   })
